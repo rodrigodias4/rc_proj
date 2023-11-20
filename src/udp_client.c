0b9fb6045cbc8798f9c6fc47b4fb6282c0bda343
Rodrigo Dias <rodrigofdias@tecnico.ulisboa.pt>
2023-11-20 15:32:02 +0000

    interpretar args

--- src/udp_client.c
+++ src/udp_client.c
@@ -15,6 +15,11 @@ struct addrinfo hints, *res;
 struct sockaddr_in addr;
 char buffer[BUF_SIZE];
 char msg[BUF_SIZE];
+char asip[32] = "localhost";
+char asport[8] = "58051";  // 58000 + group number (51)
+// tejo ip: 193.136.138.142
+// 58011 : server
+// 58001 : echo
 
 void parse_msg(char *buffer, char *msg) {
     char *temp;
@@ -34,7 +39,7 @@ void parse_msg(char *buffer, char *msg) {
     strcat(msg, "\n");
 }
 
-int main(int argc, char **argv) {
+int udp(char *msg) {
     // UDP socket
     fd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -45,33 +50,97 @@ int main(int argc, char **argv) {
     hints.ai_socktype = SOCK_DGRAM;
     // IPv4
     // UDP socket
-    errcode = getaddrinfo("193.136.138.142", "58001", &hints, &res);
+    errcode = getaddrinfo(asip, asport, &hints, &res);
     if (errcode != 0) /*error*/
         exit(1);
+
+    n = sendto(fd, msg, strlen(msg)+1, 0, res->ai_addr, res->ai_addrlen);
+    if (n == -1) /*error*/
+        exit(1);
+    addrlen = sizeof(addr);
+
+    memset(buffer, 0, BUF_SIZE);
+    n = recvfrom(fd, buffer, BUF_SIZE, 0, (struct sockaddr *)&addr, &addrlen);
+    if (n == -1) /*error*/
+        exit(1);
+
+    write(1, "echo: ", 6);
+    write(1, buffer, n);
+
+    freeaddrinfo(res);
+    close(fd);
+}
+
+int tcp(char *msg) {
+    /* Cria um socket TCP (SOCK_STREAM) para IPv4 (AF_INET).
+    É devolvido um descritor de ficheiro (fd) para onde se deve comunicar. */
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd == -1) {
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;  // TCP socket
+
+    errcode = getaddrinfo(asip, asport, &hints, &res);
+    if (errcode != 0) {
+        exit(1);
+    }
+
+    /* Em TCP é necessário estabelecer uma ligação com o servidor primeiro
+       (Handshake). Então primeiro cria a conexão para o endereço obtido através
+       de `getaddrinfo()`. */
+    n = connect(fd, res->ai_addr, res->ai_addrlen);
+    if (n == -1) {
+        exit(1);
+    }
+
+    /* Escreve a mensagem "Hello!\n" para o servidor, especificando o seu
+     * tamanho */
+    n = write(fd, msg, strlen(msg)+1);
+    if (n == -1) {
+        exit(1);
+    }
+
+    /* Lê 128 Bytes do servidor e guarda-os no buffer. */
+    n = read(fd, buffer, 128);
+    if (n == -1) {
+        exit(1);
+    }
+
+    /* Imprime a mensagem "echo" e o conteúdo do buffer (ou seja, o que foi
+    recebido do servidor) para o STDOUT (fd = 1) */
+    write(1, "echo: ", 6);
+    write(1, buffer, n);
+
+    /* Desaloca a memória da estrutura `res` e fecha o socket */
+    freeaddrinfo(res);
+    close(fd);
+}
+
+int main(int argc, char **argv) {
+    if (argc > 1) {
+        for (int i = 1; i < argc && i < 5; i += 2) {
+            if (!strcmp(argv[i], "-n") && (argc > i + 1)) {
+                strcpy(asip, argv[i + 1]);
+            }
+            if (!strcmp(argv[i], "-p") && (argc > i + 1)) {
+                strcpy(asport, argv[i + 1]);
+            }
+        }
+    }
+    printf("ip: %s\n", asip);
+    printf("port: %s\n", asport);
+    return 0;
 
     while (1) {
         // read message from terminal
         fgets(buffer, BUF_SIZE, stdin);
-        int str_len = strlen(buffer);
         memset(msg, 0, BUF_SIZE);
         parse_msg(buffer, msg);
         printf("%s\n", msg);
 
-        n = sendto(fd, msg, str_len, 0, res->ai_addr, res->ai_addrlen);
-        if (n == -1) /*error*/
-            exit(1);
-        addrlen = sizeof(addr);
-
-        memset(buffer, 0, BUF_SIZE);
-        n = recvfrom(fd, buffer, BUF_SIZE, 0, (struct sockaddr *)&addr,
-                     &addrlen);
-        if (n == -1) /*error*/
-            exit(1);
-
-        write(1, "echo: ", 6);
-        write(1, buffer, n);
+        udp(msg);
     }
-
-    freeaddrinfo(res);
-    close(fd);
 }
